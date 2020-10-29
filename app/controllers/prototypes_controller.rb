1e85ec6a5e29e37b6da3900a0b33c8cdb587db58
ryu20010202 <apo200187@gmail.com>
2020-10-29 16:02:50 +0900

    投稿機能の作成

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -5,9 +5,23 @@ class PrototypesController < ApplicationController
   end
 
   def new
+    @prototype = Prototype.new
   end
 
   def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      @prototypes = Prototype.new
+      render :new
+    end
+    
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:image, :title, :catch_copy, :concept).merge(user_id: current_user.id)
   end
 
 end
