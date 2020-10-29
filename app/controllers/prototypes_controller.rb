56d8dcafc4cef472ebc23b8cc3edc3c30db60953
ryu20010202 <apo200187@gmail.com>
2020-10-29 18:11:13 +0900

    特定投稿のshow設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,9 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:show]
 
   def index
     @user = current_user.name
+    @prototypes = Prototype.all
   end
 
   def new
@@ -19,9 +21,16 @@ class PrototypesController < ApplicationController
     
   end
 
+  def show
+  end
+
   private
   def prototype_params
     params.require(:prototype).permit(:image, :title, :catch_copy, :concept).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
   end
 
 end
